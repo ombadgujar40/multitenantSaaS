9bbba85814e532b40d6c727be4ec9030b783bd72
ombadgujar40 <ombadgujar40@gmail.com>
2025-11-22 19:28:21 +0530

    bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -41,7 +41,7 @@ const App = () => (
             <Sonner />
             <BrowserRouter>
               <Routes>
-                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/admin/login" element={<AdminLogin />} />
 
